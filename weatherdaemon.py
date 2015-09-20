caab6705d8acb7d38456d774ba0b009ad013346d
akalji <akalji@ya.ru>
2015-09-20 09:30:39 +0300

    Starts work by weatherdaemon engine

---
+++ weatherdaemon.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+import sys
+import MySQLdb
+import string
+from xml.dom.minidom import parse
+
+def getConfig():
+	global dbCitiesUser, dbCitiesPassword, dbCitiesName, dbWeatherUser, dbWeatherPassword, dbWeatherName, tabPrefix
+	XMLconf=parse("config.xml")
+	data=XMLconf.getElementsByTagName('dbCitiesUser')
+	for e in data:
+		for t in e.childNodes:
+			dbCitiesUser = t.data
+			
+	data=XMLconf.getElementsByTagName('dbCitiesPassword')
+	for e in data:
+		for t in e.childNodes:
+			dbCitiesPassword = t.data
+	
+	data=XMLconf.getElementsByTagName('dbCitiesName')
+	for e in data:
+		for t in e.childNodes:
+			dbCitiesName = t.data
+			
+	XMLconf=parse("security.xml")
+	data=XMLconf.getElementsByTagName('dbWeatherUser')
+	for e in data:
+		for t in e.childNodes:
+			dbWeatherUser = t.data
+			
+	data=XMLconf.getElementsByTagName('dbWeatherPassword')
+	for e in data:
+		for t in e.childNodes:
+			dbWeatherPassword = t.data
+	
+	data=XMLconf.getElementsByTagName('dbWeatherName')
+	for e in data:
+		for t in e.childNodes:
+			dbWeatherName = t.data
+	
+	data=XMLconf.getElementsByTagName('tabPrefix')
+	for e in data:
+		for t in e.childNodes:
+			dbWeatherName = t.data
+
+def DoQuery(qhost, quser, qpassword, qdb, qsql):
+	db = MySQLdb.connect(host=qhost, user=quser, passwd=qpassword, db=qdb, use_unicode = 1, charset='utf8')
+	db.set_character_set('utf8')
+	Cursor=db.cursor()
+	Cursor.execute('SET NAMES utf8')
+	Cursor.execute('SET CHARACTER SET utf8')
+	Cursor.execute('SET character_set_connection=utf8')
+	Cursor.execute(qsql)
+	dbCities.commit()
+	dbCities.close()
+	
+def addCity()
+	ZIPCODE = raw_input("City ZIP code: ")
+	LATINNAME = raw_input("Latin City Name: ")
+	RUSNAME = raw_input("Russian City Name: ")
+	YANDEX = raw_input("Yandex Weather Code: ")
+	GISMETEO = raw_input("Gismeteo Weather Code: ")
+	VKID = raw_input("VK Group: ")
