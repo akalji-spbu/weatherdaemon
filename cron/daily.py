24276a7499da477094fa1e6bddca41f63f876083
akalji <akalji@ya.ru>
2015-11-16 11:10:15 +0300

    New classes

--- cron/daily.py
+++ cron/daily.py
@@ -1,3 +1,64 @@
+# -*- coding:utf-8 -*-
 import sys
 sys.path.append('../')
 import weatherdaemon
+
+    for char in data:
+        if stage == 1:
+            if char == "%":
+            
+            else:
+                if char == "*":
+                
+                else:
+                    if char == "a"
+                    
+        else:
+            if stage == 2:
+                if char == "%":
+            
+                else:
+                    if char == "*":
+                
+                    else:
+                        if char == "a"
+                        
+            else:
+                if stage == 3:
+                    if char == "%":
+            
+                    else:
+                        if char == "*":
+                
+                        else:
+                            if char == "a"
+                    
+                else:
+                    if stage == 4:
+                        if char == "%":
+            
+                        else:
+                            if char == "*":
+                
+                            else:
+                                if char == "a"
+                    
+                    else:
+                        if stage == 5:
+                            if char == "%":
+            
+                            else:
+                                if char == "*":
+                
+                                else:
+                                    if char == "a"
+                    
+                        else:
+                            if stage == 6:
+                                if char == "%":
+            
+                                else:
+                                    if char == "*":
+                
+                                    else:
+                                        if char == "a"
--- weatherdaemon.py
+++ weatherdaemon.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import string
+from datetime import datetime
 import configparser
 from xml.dom.minidom import parse
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
@@ -40,8 +41,9 @@ class City(Base):
     RegionCode  = Column(Integer)
     CountryCode = Column(Integer)
     Resort      = Column(Integer)
+    TimeZone    = Column(Integer)
         
-    def __init__(self, CityCode = 0, EnglishName = 0, LocalName = 0, ZipCode = 0, RegionCode = 0, CountryCode = 0, Resort = 0):
+    def __init__(self, CityCode = 0, EnglishName = 0, LocalName = 0, ZipCode = 0, RegionCode = 0, CountryCode = 0, Resort = 0, TimeZone = 0):
         if CityCode==0:
             self.CityCode       = input("International Weather Code: ") 
             self.EnglishName    = input("City Name in English: ")
@@ -49,7 +51,9 @@ class City(Base):
             self.ZipCode        = input("City ZIP code: ")
             self.RegionCode     = input("City Region code: ")
             self.CountryCode    = input("City Country code: ")
-            self.Resort         = input("City resort status (true or false): ")
+            self.Resort         = input("City resort status (True or False): ")
+            self.TimeZone       = input("Citi's timezone is (from -12 to +12): ")         
+            #self.latitude, self.longitude = GetCoordinates()
         else:
             self.CityCode       = CityCode
             self.EnglishName    = EnglishName 
@@ -58,8 +62,12 @@ class City(Base):
             self.RegionCode     = RegionCode
             self.CountryCode    = CountryCode
             self.Resort         = Resort
+            self.TimeZone       = TimeZone
+            #self.latitude, self.longitude = GetCoordinates()
 
-
+    def __repr__(self):
+        return "<City(CityCode='%s', EnglishName='%s', LocalName='%s', ZipCode='%s', RegionCode='%s', CountryCode='%s', Resort='%s', TimeZone='%s')>" % (self.CityCode, self.EnglishName, self.LocalName, self.ZipCode, self.RegionCode, self.CountryCode, self.Resort, self.TimeZone)
+    
 
 class Region(Base):
     __tablename__ = 'regions'
@@ -79,7 +87,10 @@ class Region(Base):
             self.EnglishName    = EnglishName
             self.LocalName      = LocalName
             self.CountryCode    = CountryCode
-        
+    def __repr__(self):
+        return "<Region(RegionCode='%s', EnglishName='%s', LocalName='%s', CountryCode='%s')>" % (self.RegionCode, self.EnglishName, self.LocalName, self.CountryCode)
+    
+   
     
 class Country(Base):
     __tablename__ = 'countries'
@@ -96,10 +107,104 @@ class Country(Base):
             self.CountryCode    = CountryCode
             self.EnglishName    = EnglishName
             self.LocalName      = LocalName
+    def __repr__(self):
+        return "<Country(CountryCode='%s', EnglishName='%s', LocalName='%s')>" % (self.CountryCode, self.EnglishName, self.LocalName)
+    
 
-#end core classes
+class VK_g(Base):
+    __tablename__ = 'vk_subs'
+    sub_id        = Column(Integer, primary_key=True)
+    CityCode      = Column(String(30))
+    GroupID       = Column(String(30))
+    Expired       = Column(Integer)
+    APIkey        = Column(String(30))
+    User          = Column(String(30))
+    Password      = Column(String(30))
+    
+    def __init__(self, sub_id = 0, CityCode = 0, GroupID = 0, Expired = 0, APIkey = 0, User = 0, Password = 0):
+        if RegionCode==0:
+            self.CityCode   = input(": ")
+            self.GroupID    = input(": ")
+            self.Expired    = input(": ")
+            self.APIkey     = input(": ")
+            self.User       = input(": ")
+            self.Password   = input(": ")
+        else:
+            self.CityCode   = CityCode
+            self.GroupID    = GroupID
+            self.Expired    = Expired
+            self.APIkey     = APIkey
+            self.User       = User
+            self.Password   = Password
+    
+    
+    def __repr__(self):
+        return "<VK(sub_id='%s', CityCode='%s', GroupID='%s', Expired='%s', APIkey='%s', User='%s', Password='%s')>" % (self.sub_id, self.CityCode, self.GroupID, self.Expired, self.APIkey, self.User, self.Password)
+            
+class Forecast(Base):
+    __tablename__   = 'forecast'
+    Timestamp       = Column(Integer, primary_key=True)
+    CityCode        = Column(Integer)
+    Cloudiness      = Column(Integer)
+    Precipitation   = Column(Integer)
+    Rpower          = Column(Integer)
+    Spower          = Column(Integer)
+    Pressure        = Column(Integer)
+    Tempirature     = Column(Integer)
+    WindMin         = Column(Integer)
+    WindMax         = Column(Integer)
+    WindDirection   = Column(Integer)
+    Relwet          = Column(Integer)
+    Heat            = Column(Integer)
+    Sunrise         = Column(Integer)
+    Sunset          = Column(Integer)
+    Moonrise        = Column(Integer)
+    Moonset         = Column(Integer)
+    Moonphase       = Column(Integer)
+    
+    def __init__(self, Timestamp = 0, CityCode = 0, Cloudiness = 0, Precipitation = 0, Rpower = 0, Spower = 0, Pressure = 0, Tempirature = 0, WindMin = 0, WindMax = 0, WindDirection = 0, Relwet = 0, Heat = 0, Sunrise = 0, Sunset = 0, Moonrise = 0, Moonset = 0, Moonphase = 0):
+        if CityCode==0:
+            self.Timestamp      = Timestamp
+            self.CityCode       = CityCode
+            self.Cloudiness     = Cloudiness
+            self.Precipitation  = Precipitation
+            self.Rpower         = Rpower
+            self.Spower         = Spower
+            self.Pressure       = Pressure
+            self.Tempirature    = Tempirature
+            self.WindMin        = WindMin
+            self.WindMax        = WindMax
+            self.WindDirection  = WindDirection
+            self.Relwet         = Relwet
+            self.Heat           = Heat
+            self.Sunrise        = Sunrise
+            self.Sunset         = Sunset
+            self.Moonrise       = Moonrise
+            self.Moonset        = Moonset
+            self.Moonphase      = Moonphase
+        else:
+            self.Timestamp      = Timestamp()
+            self.CityCode       = input(": ")
+            self.Cloudiness     = input(": ")
+            self.Rpower         = input(": ")
+            self.Spower         = input(": ")
+            self.Pressure       = input(": ")
+            self.Tempirature    = input(": ")
+            self.WindMin        = input(": ")
+            self.WindMax        = input(": ")
+            self.WindDirection  = input(": ")
+            self.Relwet         = input(": ")
+            self.Heat           = input(": ")
+            self.Sunrise        = input(": ")
+            self.Sunset         = input(": ")
+            self.Moonrise       = input(": ")
+            self.Moonset        = input(": ")
+            self.Moonphase      = input(": ")
 
-#begin setup fucntions
+    
+    def __repr__(self):
+        return "<Forecast(Timestamp='%s', CityCode='%s', Cloudiness='%s', Precipitation='%s', Rpower='%s', Spower='%s', Pressure='%s', Tempirature='%s', WindMin='%s', WindMax='%s', WindDirection='%s', Relwet='%s', Heat='%s', Sunrise='%s', Sunset='%s', Moonrise='%s', Moonset='%s', Moonphase='%s')>" % (self.Timestamp, self.CityCode, self.Cloudiness, self.Precipitation, self.Rpower, self.Spower, self.Pressure, self.Tempirature, self.WindMin, self.WindMax, self.WindDirection, self.Relwet, self.Heat, self.Sunrise, self.Sunset, self.Moonrise, self.Moonset, self.Moonphase)
+
 def setup():
     ConfigFile = "config.ini"
     try:
@@ -117,7 +222,7 @@ def setup():
         print("Configuration alredy exists, do you want replace it?")
         cf = input("(yes/no)?: ")
         if cf == "yes":
-            print("1) MySQL", "2) PgSQL", sep='\t')
+            print("1) MySQL", "2) PgSQL, 3) MSSQL", sep='\t')
             DBtype   = input("Select db type: ")
             DBserver   = input("Database server IP or name: ")
             DBport     = input("Database service port: ")
@@ -136,7 +241,7 @@ def setup():
                 if TestConnection(config.DBengine,config.DBserver, config.DBport, config.DBuser, config.DBpassword, config.DBname):
                     print("Database alredy exists.")
                     initDB()
-                    raise SystemExit("Database successful initialised!")
+                raise Dead("Database successful initialised!")
                     
         if DBtype == 1:
             DBengine="mysql+pymysql://"
@@ -193,4 +298,109 @@ def Dead(error_reason):
     
     if error_reason == 0:   #Normal exit code
         print("Good Bye!")
-        raise SystemExit("error_reason")
+    raise SystemExit("error_reason")
+
+def GetOneWeather(CitiCode):
+    Gismeteo    = ParseGismeteo(CitiCode)
+    Yandex      = ParseYandex(CitiCode)
+    Sunrise     = Sunrise(CitiCode)
+    Sunset      = Sunset(CitiCode)
+    Moonrise    = Moonrise(CitiCode)
+    Moonset     = Moonset(CitiCode)
+    Moonphase   = Moonphase(CitiCode)
+    Weather     = Forecast(CityCode, (Gismeteo.Cloudiness+Yandex.Cloudiness)/2, (Gismeteo.Precipitation+Yandex.Precipitation)/2, (Gismeteo.Rpower+Yandex.Rpower)/2, (Spower.Cloudiness+Spower.Cloudiness)/2, (Gismeteo.Pressure+Yandex.Pressure)/2, (Gismeteo.Tempirature+Yandex.Tempirature)/2, (Gismeteo.WindMin+Yandex.WindMin)/2, (Gismeteo.WindMax+Yandex.WindMax)/2, (Gismeteo.WindDirection+Yandex.WindDirection)/2, (Gismeteo.Relwet+Yandex.Relwet)/2, (Gismeteo.Heat+Yandex.Heat)/2, Sunrise, Sunset, Moonrise, Moonset, Moonphase)    
+    
+def ParseGismeteo(CitiCode):
+    URL = "http://informer.gismeteo.ru/xml/"+CitiCode+"_1.xml"
+    
+    Cloudiness
+    Precipitation
+    Rpower
+    Spower
+    Pressure
+    Tempirature
+    WindMin
+    WindMax
+    WindDirection
+    Relwet
+    Heat
+    Sunrise = 0
+    Sunset = 0
+    Moonrise  = 0
+    Moonset = 0
+    Moonphase = 0
+    Forecast = Forecast(Timestamp(), CityCode, Cloudiness, Precipitation, Rpower, Spower, Pressure, Tempirature, WindMin, WindMax, WindDirection, Relwet, Heat, Sunrise, Sunset, Moonrise, Moonset, Moonphase)
+    return Forecast
+    
+def ParseYandex(CitiCode):
+    URL = "https://export.yandex.ru/weather-ng/forecasts/"+CitiCode+".xml"
+    
+    Cloudiness
+    Precipitation
+    Rpower
+    Spower
+    Pressure
+    Tempirature
+    WindMin
+    WindMax
+    WindDirection
+    Relwet
+    Heat
+    Sunrise = 0
+    Sunset = 0
+    Moonrise = 0
+    Moonset = 0
+    Moonphase = 0
+    Forecast = Forecast(Timestamp(), CityCode, Cloudiness, Precipitation, Rpower, Spower, Pressure, Tempirature, WindMin, WindMax, WindDirection, Relwet, Heat, Sunrise, Sunset, Moonrise, Moonset, Moonphase)
+    return Forecast
+
+def Sunrise(CitiCode): 
+    Sunrise = 0
+    return Sunrise
+
+def Sunset(CitiCode):
+    Sunset = 0
+    return Sunset
+
+def Moonrise(CitiCode):
+    Moonrise = 0
+    return Moonrise
+
+def Moonset(CitiCode): 
+    Moonset = 0
+    return Moonset
+
+def Moonphase(CitiCode):
+    Moonphase = 0
+    return Moonphase
+
+def ExportToXLS(Query, TableTuple, File):
+    print("exportDone")
+
+
+def ExportToMXL(Query, TableTuple, File):
+    print("exportDone")
+
+def ExportToXML(Query, TableTuple, File):
+    print("exportDone")
+    
+def Timestamp():
+    timestamp = 0
+    return timestamp
+def PostToVK(Image, VKID)
+    print("exportDone")
+
+def Logger():   #ассинхронная функция
+    print("LoggingIsStarted")
+
+def GenerateImage(Forecast)
+    return Base64(Image)
+
+def GetCoordinates()
+    return latitude, longitude
+
+def clrscr():
+    try:
+        os.system('clear')
+    except:
+        os.system('cls')
